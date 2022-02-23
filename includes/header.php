8c4f9eeb887d472067481993db7becc14d01e841
ndavido <D00239107@student.dkit.ie>
2022-02-23 18:48:49 +0000

    Added scss

--- includes/header.php
+++ includes/header.php
@@ -1,7 +1,8 @@
 <!-- the head section -->
 <head>
 <title>My PHP CRUD App</title>
-<link rel="stylesheet" type="text/css" href="main.css">
+<link rel="stylesheet" type="text/css" href="css/main.css">
+
 </head>
 
 <!-- the body section -->
