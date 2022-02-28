7eb5902b1800f844ed9f5206cb60197c0de19478
ndavido <D00239107@student.dkit.ie>
2022-02-28 12:24:10 +0000

    Making a nav bar

--- includes/header.php
+++ includes/header.php
@@ -1,10 +1,35 @@
 <!-- the head section -->
+
 <head>
-<title>My PHP CRUD App</title>
-<link rel="stylesheet" type="text/css" href="css/main.css">
+    <title>My PHP CRUD App</title>
+    <link rel="stylesheet" type="text/css" href="css/main.css">
 
 </head>
 
 <!-- the body section -->
+
 <body>
-<header><h1>My PHP CRUD app</h1></header>
+    <header>
+        <h1>My PHP CRUD app</h1>
+        <div class="topnav">
+            <a class="active" href="#home">Home</a>
+            <a href="#about">About</a>
+            <a href="#contact">Contact</a>
+            <div class="search-container">
+                <form action="/action_page.php">
+                    <input type="text" placeholder="Search.." name="search">
+                    <button type="submit"><i class="fa fa-search"></i></button>
+                </form>
+            </div>
+        </div>
+        <nav>
+            <ul>
+                <?php foreach ($categories as $category) : ?>
+                    <li><a href=".?category_id=<?php echo $category['categoryID']; ?>">
+                            <?php echo $category['categoryName']; ?>
+                        </a>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
+        </nav>
+    </header>
