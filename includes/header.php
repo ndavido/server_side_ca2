5518f470e44ef7169c9680cb9c5fdb9186af106c
ndavido <D00239107@student.dkit.ie>
2022-02-28 12:45:09 +0000

    changes in css

--- includes/header.php
+++ includes/header.php
@@ -3,7 +3,7 @@
 <head>
     <title>My PHP CRUD App</title>
     <link rel="stylesheet" type="text/css" href="css/main.css">
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 
 <!-- the body section -->
