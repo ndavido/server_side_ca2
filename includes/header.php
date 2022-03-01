114dd9b5075e730ce8d0b27a3d35d8ff03094d20
ndavido <D00239107@student.dkit.ie>
2022-03-01 21:58:08 +0000

    navbar fixes

--- includes/header.php
+++ includes/header.php
@@ -11,12 +11,13 @@
 <body>
     <header>
         <h1>Job Application</h1>
-        <div class="topnav">
-            <a class="active" href="#home">Home</a>
-            <a href="#about">About</a>
-            <a href="#contact">Contact</a>
+        <div class="navbar">
+            <a href="#home">Home</a>
+            <a href="#news">News</a>
             <div class="dropdown">
-                <button class="dropbtn">Dropdown</button>
+                <button class="dropbtn">Dropdown
+                    <i class="fa fa-caret-down"></i>
+                </button>
                 <div class="dropdown-content">
                     <?php foreach ($categories as $category) : ?>
                         <a href=".?category_id=<?php echo $category['categoryID']; ?>">
@@ -25,21 +26,5 @@
                     <?php endforeach; ?>
                 </div>
             </div>
-            <div class="search-container">
-                <form action="/action_page.php">
-                    <input type="text" placeholder="Search.." name="search">
-                    <button type="submit"><i class="fa fa-search"></i></button>
-                </form>
-            </div>
         </div>
-        <nav>
-            <ul>
-                <?php foreach ($categories as $category) : ?>
-                    <li><a href=".?category_id=<?php echo $category['categoryID']; ?>">
-                            <?php echo $category['categoryName']; ?>
-                        </a>
-                    </li>
-                <?php endforeach; ?>
-            </ul>
-        </nav>
     </header>
